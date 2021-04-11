0b50f95ff3eb8eb03a36afa3013cbe0c3f739222
Ricard Monge Calvo <ricard.monge@basetis.com>
2021-04-12 01:24:55 +0200

    Add tests folder with example tests.

---
+++ tests/examples/test_routenet.py
@@ -0,0 +1,15 @@
+import ignnition
+import pytest
+from tests.utils import working_directory
+
+example_dir = "examples/Routenet"
+
+@pytest.mark.timeout(60*5)  # Timeout after 5 minutes
+def test_routenet():
+    """Test Routenet example"""
+    with working_directory(example_dir):
+        model = ignnition.create_model(model_dir= './')
+        # Set epochs to a limited amount for time constraints
+        model.CONFIG["epochs"] = 1
+        model.computational_graph()
+        model.train_and_validate()
---
+++ tests/utils.py
@@ -0,0 +1,20 @@
+import os
+from contextlib import contextmanager
+
+@contextmanager
+def working_directory(path):
+    """
+    A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
+    Usage:
+    > # Do something in original directory
+    > with working_directory('/my/new/path'):
+    >     # Do something in new directory
+    > # Back to old directory
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
